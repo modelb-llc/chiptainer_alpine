d541415a621abed44ee23f58d1d0f433ec7d846a
Alex Kaplan <kaplan2539@gmail.com>
2017-02-02 15:56:51 -0800

    Added qemu-arm-static

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM scratch
 
 ADD rootfs.tar.gz /
+ADD qemu-arm-static.tar.gz /
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 CONTAINER_IMAGE=${CONTAINER_IMAGE:-nextthingco/chiptainer_alpine}
+
 ROOTFS_URL=${ROOTFS_URL:-https://nl.alpinelinux.org/alpine/v3.5/releases/armhf/alpine-minirootfs-3.5.0-armhf.tar.gz}
+QEMU_STATIC_URL=${QEMU_STATIC_URL:-http://kaplan2539.gitlab.io/baumeister/qemu-arm-static.tar.gz}
 
 wget -c $ROOTFS_URL -O rootfs.tar.gz
+wget -c $QEMU_STATIC_URL
 docker build -t "${CONTAINER_IMAGE}" .
